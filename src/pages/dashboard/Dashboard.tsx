9939ec8f15fc363f8e11acc087bf0cb765b33b8e
LuanRoch <lr.prazeres@bol.com.br>
2024-08-15 22:24:44 -0300

    feat: api

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,24 +1,158 @@
-import { Button, TextField } from "@mui/material";
-import {FerramentaDeDetalhes} from "../../shared/components";
+import { Box, Button, Grid, Icon, Input, Paper, TextField, Typography, useTheme } from "@mui/material";
+import { FerramentaDeDetalhes } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layout";
-import { Typography } from "@mui/material/styles/createTypography";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+
+
 
 
 export const Dashboard = () => {
+const checkCNPJ = (e:any) => {
+    if (!e.target.value){
+        return;
+    }
+    const cnpj = e.target.value.replace(/[^\d]+/g,"")
+
+}
 
 
-    return(
-       <LayoutBaseDePagina 
-       titulo="Pagina inicial" 
-       BarraDeFerramenta = {(
-        <FerramentaDeDetalhes mostrarBotaoSalvarEFechar mostrarBotaoNovo 
-        
-        />
-       )}>
-        <TextField
-                    placeholder=""
+    const [cnpj, SetCnpj] = useState([])
+    const getPosts = async () => {
+
+        try {
+            const response = await axios.get("https://brasilapi.com.br/api/cnpj/v1/${cnpj}"
+
+            );
+            const data = response.data
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+    useEffect(() => {
+
+        getPosts()
+    }, [])
+
+    return (
+        <LayoutBaseDePagina
+            titulo="Consultar CNPJ"
+            BarraDeFerramenta={(
+                <FerramentaDeDetalhes mostrarBotaoSalvarEFechar mostrarBotaoNovo
+
                 />
-      
-      </LayoutBaseDePagina> 
+
+
+            )}>
+            <Box
+                gap={1}
+                marginX={1}
+                padding={1}
+                paddingX={1}
+                display='grid'
+                alignItems='center'
+                component={Paper}>
+
+                <Box 
+                flex={1} 
+                display='flex' 
+                justifyContent='start' 
+                gap={1}
+                component="form"
+                    >
+                    
+
+                    {(
+                        <TextField id="outlined-basic" label="CNPJ:" variant="outlined" value={cnpj} />
+
+
+                    )}
+                    {(
+                        <Button
+                            color="primary"
+                            disableElevation
+                            variant="contained"
+                            onClick={checkCNPJ}
+                            endIcon={<Icon>send</Icon>}>
+                            Buscar
+                        </Button>
+                    )}
+
+                </Box>
+
+
+                <Box
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+
+
+                    <div>
+                        <TextField
+                            disabled
+                            id="outlined-disabled"
+                            label="Nome"
+                            defaultValue=""
+                        />
+                        <TextField
+                            disabled
+                            id="outlined-disabled"
+                            label="Razão Social"
+                            defaultValue=""
+                        />
+                        <TextField
+                            disabled
+                            id="outlined-disabled"
+                            label="Data de Abertura"
+                            defaultValue=""
+                        />
+                        <TextField
+                            disabled
+                            id="outlined-disabled"
+                            label="Situação"
+                            defaultValue=""
+                        />
+                        <TextField
+                            disabled
+                            id="outlined-disabled"
+                            label="Atividade Principal"
+                            defaultValue=""
+                        />
+                        <TextField
+                            disabled
+                            id="outlined-disabled"
+                            label="Endereço Completo"
+                            defaultValue=""
+                        />
+                        <TextField
+                            disabled
+                            id="outlined-disabled"
+                            label="Telefone"
+                            defaultValue=""
+                        />
+                        <TextField
+                            disabled
+                            id="outlined-disabled"
+                            label="E-mail"
+                            defaultValue=""
+                        />
+                    </div>
+
+
+
+                </Box>
+
+
+            </Box>
+
+        </LayoutBaseDePagina>
     );
 }
